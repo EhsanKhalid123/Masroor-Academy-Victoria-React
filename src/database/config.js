95cb1f6a9a9f1f262bd2ac45b8eb0f1bd7579c83
Ehsan Khalid <s3838975@student.rmit.edu.au>
2022-08-15 22:46:36 +1000

    Added .env & Build Ready

---
+++ src/database/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+// console.log(process.env)
+
+module.exports = {
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
+  DIALECT: process.env.DIALECT
+};
