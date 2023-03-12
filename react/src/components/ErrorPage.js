a3da0cbac653f3001f3a5e9903439d2045f2dfa2
Ehsan Khalid <s3838975@student.rmit.edu.au>
2023-03-12 21:31:44 +1100

    Fixed Unresponsive Error 404 Page

--- react/src/components/ErrorPage.js
+++ react/src/components/ErrorPage.js
@@ -1,20 +1,20 @@
-import '../Error404.css';
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
-  
-  
-    return (
-        <div>
-      <h1 className="text-center" style={{ paddingTop: "50px" }}>ERROR</h1>
 
-    <div className="mainbox">
-      <div className="err">4</div>
-      <i className="text-center far fa-question-circle fa-spin"></i>
-      <div className="err2">4</div>
-      <div className="msg">
-        Sorry the page does not exist!
+
+  return (
+    <div>
+      <br />
+      <div className="container">
+        <h1 className="text-center">ERROR</h1>
+        <div className="error-container">
+          <div className="error-number">4</div>
+          <i className="far fa-question-circle fa-spin"></i>
+          <div className="error-number">4</div>
+        </div>
+        <div className="error-message">Sorry the page does not exist! <Link className="navbar-brand" to="/">Go to Homepage </Link></div>
       </div>
-    </div>
     </div>
   );
 }
