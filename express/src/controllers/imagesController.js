bc1d54660b85a2b670ca723fe8ee282a702a2fc1
Ehsan Khalid <s3838975@student.rmit.edu.au>
2023-03-21 22:26:58 +1100

    Fixed Image Upload Not Working in Text Editor for Announcements & Changed some styling

--- express/src/controllers/imagesController.js
+++ express/src/controllers/imagesController.js
@@ -3,24 +3,24 @@
 const db = require("../database");
 const fs = require('fs');
 
-// exports.upload = async (req, res) => {
-//   const { path } = req.file;
-//   try {
-//     const data = await db.images.create({
-//       name: req.file.originalname,
-//       data: fs.readFileSync(path),
-//     });
-//     console.log(data);
-//     res.status(200).json({
-//       message: "Image uploaded successfully!",
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({
-//       message: "Error uploading image!",
-//     });
-//   }
-// };
+exports.upload = async (req, res) => {
+  const { path } = req.file;
+  try {
+    const data = await db.images.create({
+      name: req.file.originalname,
+      data: fs.readFileSync(path),
+    });
+    console.log(data);
+    res.status(200).json({
+      message: "Image uploaded successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error uploading image!",
+    });
+  }
+};
 
 // exports.upload = async (req, res) => {
 //     try {
@@ -48,24 +48,24 @@ const fs = require('fs');
 
 
 // Endpoint for uploading an image and saving it in the database.
-exports.upload = async (req, res) => {
-    try {
-      // Get the uploaded image file.
-    //   const imageFile = req.files.file;
-      const imageFile = req.file;
+// exports.upload = async (req, res) => {
+//     try {
+//       // Get the uploaded image file.
+//     //   const imageFile = req.files.file;
+//       const imageFile = req.file;
   
-      // Save the image file to the database.
-      const image = await db.images.create({
-        Image: imageFile.data
-      });
+//       // Save the image file to the database.
+//       const image = await db.images.create({
+//         Image: imageFile.data
+//       });
   
-      // Construct the image URL using the ID of the saved image.
-      const imageUrl = `/MAApi/image/${image.id}`;
+//       // Construct the image URL using the ID of the saved image.
+//       const imageUrl = `/MAApi/image/${image.id}`;
   
-      // Return the image URL as a response.
-      res.json({ url: imageUrl });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Failed to upload image." });
-    }
-  };
+//       // Return the image URL as a response.
+//       res.json({ url: imageUrl });
+//     } catch (err) {
+//       console.error(err);
+//       res.status(500).json({ error: "Failed to upload image." });
+//     }
+//   };
